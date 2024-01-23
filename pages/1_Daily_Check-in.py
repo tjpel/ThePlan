908267411b2003c41dfbae2c099477604193edd4
Thomas Pelowitz <tjp00004@gmail.com>
2024-01-23 11:54:07 -0600

    Init

--- pages/1_Daily_Check-in.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas
-from datetime import date
-
-st.title("Daily Check-in!")
-today = date.today().strftime("%d/%m/%Y")
-print(today)
-st.subheader("Today's Date is: " + today)
