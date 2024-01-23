d970cc619b7bb4a67741efc6c29b773fc68b4c97
Thomas Pelowitz <tjp00004@gmail.com>
2024-01-23 11:58:14 -0600

    Init

--- pages/1_Daily_Check-in.py
+++ pages/1_Daily_Check-in.py
@@ -7,16 +7,19 @@ today = date.today().strftime("%d/%m/%Y")
 st.subheader("Today's Date is: " + today)
 
 with st.form(key='daily_checkin'):
-    mental_health = st.slider("How was your mental health today?")
-    st.text("Any Expenses Today?")
+    mental_health = st.slider("How was your mental health today?", 1, 5)
 
     checkin_submitted = st.form_submit_button("Submit")
     if checkin_submitted:
         pass
 
 with st.form(key="gym"):
-    gym_withProgram = st.checkbox("Went to gym and did all of expected routine")
-    gym_noProgram = st.checkbox("I did something to work out today but not what the plan was")
+    gym_activity = st.radio("What did you do today?", [
+        "Stuck to The Plan",
+        "Active, but not in line with The Plan",
+        "Nothing"
+    ])
+
     gym_minutes = st.number_input("Number of active minutes")
     gym_caloriesBurned = st.number_input("Number of Calories burned")
     gym_caloriesConsumed = st.number_input("Number of Calories consumed")
